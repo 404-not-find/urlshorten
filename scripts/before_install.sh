966a850bef95ca0d3e589d66aa2fa6adb276c9b7
焦兴伟 <xingweijiao@creditease.cn>
2019-06-30 22:16:49 +0800

    update scripts for path

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 ls -alth
 echo `pwd`
-cd /home/ec2-user/web/
-sudo chmod 777 start_server.sh
-sudo chmod 777 url-shortener-0.0.1-SNAPSHOT.jar
+sudo chmod 777 /home/ec2-user/web/start_server.sh
+sudo chmod 777 /home/ec2-user/web/url-shortener-0.0.1-SNAPSHOT.jar
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -7,4 +7,4 @@ echo $pid
 if [ $pid ];then
   kill -9 $pid
 fi
-nohup java -jar $jarname  -Xms 512m -Xmx512m > server.log 2>&1 &
+cd /home/ec2-user/web/ && nohup java -jar $jarname  -Xms 512m -Xmx512m > server.log 2>&1 &
