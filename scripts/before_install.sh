2e0256c80137d2beddd5ede18054c5d10982e11c
焦兴伟 <xingweijiao@creditease.cn>
2019-06-30 22:27:23 +0800

    update script

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-ls -alth
-echo `pwd`
+sudo mkdir -p /home/ec2-user/web/data
 sudo chmod 777 /home/ec2-user/web/start_server.sh
 sudo chmod 777 /home/ec2-user/web/url-shortener-0.0.1-SNAPSHOT.jar
