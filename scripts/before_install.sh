5e73ef72e6752dbce374108c89a230da69b507da
焦兴伟 <xingweijiao@creditease.cn>
2019-06-30 23:02:00 +0800

    update...

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo mkdir -p /home/ec2-user/web/data
-sudo chmod 777 /home/ec2-user/web/start_server.sh
-sudo chmod 777 /home/ec2-user/web/url-shortener-0.0.1-SNAPSHOT.jar
+cd /home/ec2-user/web/ && sudo chmod 777 start_server.sh
+cd /home/ec2-user/web/ && sudo chmod 777 url-shortener-0.0.1-SNAPSHOT.jar
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 jarname='url-shortener-0.0.1-SNAPSHOT.jar'
 pid=`ps aux |grep $jarname |grep -v grep |awk '{print $2}'`
-echo $pid
+echo "find pid $pid,kill it"
 if [ $pid ];then
   kill -9 $pid
 fi
 cd /home/ec2-user/web/ && nohup java -jar $jarname  -Xms 512m -Xmx512m > server.log 2>&1 &
+echo "current directory is `pwd`,server started successfully..."
